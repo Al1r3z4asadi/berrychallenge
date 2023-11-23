284dfcfc22bd01893618a2a2905564dc4c283dec
alireza asadi <ali.s.asadi98@gmail.com>
2023-11-23 22:16:42 +0300

    test project and dockerignore added

---
+++ BerrryTest/Customer/CustomerTest.cs
@@ -0,0 +1,16 @@
+namespace BerrryTest.Customer
+{
+    public class CustomerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
